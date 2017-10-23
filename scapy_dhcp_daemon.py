084da51e8e2783baba0c7670f3bad77e6a515f4f
iamhyc <sudofree@163.com>
2017-10-23 17:36:07 +0800

    almost done; NAK unsolved

--- scapy_dhcp_daemon.py
+++ scapy_dhcp_daemon.py
@@ -1,52 +1,188 @@
 #! /usr/bin/python
-
 from scapy.all import *
-from time import ctime,sleep
+from time import sleep
 import multiprocessing
-import json, socket
+import socket
+
+global udp_rx_setup, udp_tx_setup
+global null_ip, bc_ip, bc_mac, lo_mac
+global dhcp_bc_header
+global skt, proc_map, ops_map
+
+UDP_BASE_BOOTP_HEADER = 8
+BOOTP_BASE_XID_Offset = 4
+BOOTP_BASE_XID_Length = 4
 
 class DHCP_Proxy(multiprocessing.Process):
-	"""Non-Blocking running DHCP_Proxy Thread
+	"""Non-Blocking running DHCP_Proxy Process
 		@desc 
 		@param (int)state:
-			0: Ready; 4: ACK_rx && Maintain State;
-			1: Discover_tx; 2:Offer_rx; 3:Request_tx;
-		@param (dict)param
-			store the {ip, mac, mask, gateway, ttl}
+			0: Ready; 1: Discover(tx); 3: Request(tx);
+			2: Offer(rx); 4: ACK(rx) && Maintain State;
+		@var (dict)param
+			store the {ip, mac, mask, gateway}
 			information
-		@func request:
-			perform the four-phase procedure for DHCP
-			proxy request
-		@func release:
-			(not implemented yet)
-		@func 
+		@var (dict)state_map
+		@func dhcp_init:
+			initialize the `param` dict
+		@func append2file:
+		@func bootp_parse:
+		@func dhcp_discover:
+		@func dhcp_request:
 	"""
-	def __init__(self, task_id):
-		threading.Thread.__init__(self)
+
+	def __init__(self, task_id, skt_lock):
+		multiprocessing.Process.__init__(self)
 		self.id = task_id
-		self.state = 0#ready state
+		self.lock = skt_lock
+		self.state_map = {
+			0 : self.dhcp_init,
+			1 : self.dhcp_discover,
+			3 : self.dhcp_request
+		}
+		self.state = 0
+		pass
+
+	def dhcp_init(self):
+		self.param = {
+			'__filled__' : 0,
+			'__acked__' : 0,
+			'ip' : bc_ip,
+			'mac' : RandMAC("00:28:f8:7c"),#allocate a pseudo mac address
+			'gateway' : bc_ip,
+			'mask' : bc_ip
+		}
+		self.state = 1
+		pass
+
+	def append2file(self):
+		pass
+
+	def dhcp_ack_parse(self, pkt):
+		#self.state = 4
+		self.param['__acked__'] = 1;
+		pass
+
+	def dhcp_offer_parse(self, pkt):
+		#self.state = 2
+		self.param['gateway'] = pkt[DHCP].options[1][1]#server_id
+		self.param['ip'] = pkt[BOOTP].yiaddr#Offered IP
+		self.param["mask"] = pkt[DHCP].options[3][1]#subnet_mask
+		#pkt[DHCP].options[2][1]#lease_time
+		#pkt[DHCP].options[5][1]#name_server
+		self.param['__filled__'] = 1;
+		pass
+
+	def dhcp_request(self):
+		xid_tmp = random.randint(1, 900000000)
+		mac_str = mac2str(self.param["mac"])
+		request_pkt = (
+			dhcp_bc_header/ \
+			BOOTP(op=1, chaddr=mac_str, xid=xid_tmp)/ \
+        	DHCP(options=[('message-type','request'),
+        				  ('client_id', mac_str),
+        				  ('requested_addr', self.param["ip"]),
+        				  ('server_id', self.param["gateway"]),
+        				  ('end')])
+		)
+		set_filter = ('udp and udp[%d:%d]=%d'%(12, 4, xid_tmp))
+
+		sendp(request_pkt, iface=conf.iface)
+		sniff(iface=conf.iface, \
+			  filter=set_filter, \
+			  count=1, timeout=5, \
+			  prn=self.dhcp_ack_parse)
+		pass
+
+	def dhcp_discover(self):
+		xid_tmp = random.randint(1, 900000000)
+		discover_pkt = (
+			dhcp_bc_header/ \
+			BOOTP(chaddr=[mac2str(self.param["mac"])], xid=xid_tmp, flags=0xFFFFFF)/ \
+			DHCP(options=[('message-type','discover'), 
+				  ('end')])
+		)
+		set_filter = ('udp and udp[%d:%d]=%d'%(12, 4, xid_tmp))
+
+		sendp(discover_pkt, iface=conf.iface)
+		sniff(iface=conf.iface, \
+			  filter=set_filter, \
+			  count=1, timeout=5, \
+			  prn=self.dhcp_offer_parse)
+
+		if self.param['__filled__']:
+			self.state = 3
+		else:
+			self.state = 1
+		pass
 
 	def run(self):
-		#time.sleep(20)
-	
-	def request():
+		while True:
+			self.state_map[self.state]()
 		pass
 
-	def release():
-		pass
 
+
+def request(task_id, skt_lock):
+	if proc_map.has_key(task_id):
+		raise Exception
+
+	proc_map[task_id] = DHCP_Proxy(task_id, skt_lock)
+	proc_map[task_id].start()
+	pass
+
+def release(task_id, skt_lock):
+	if not proc_map.has_key(task_id):
+		raise Exception
+
+	#proc_map[task_id].join() # wait for itself exit
+	proc_map[task_id].terminate() #forcely exit
+	del proc_map[task_id] # delete the item
+	pass
 
 def main():
-	thread_map = {}
+	#single command Receive socket setup
+	skt_cmd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	skt_cmd.bind(udp_rx_setup)
+	#multi-Send socket setup wit Lock
 	skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	skt.bind(('localhost', 11112))
+	skt_lock = multiprocessing.Lock();
 
 	while True:
-		print('Waiting for frontend call...')
-    	data, addr = skt.recvfrom(1024)
-
+		data, addr = skt_cmd.recvfrom(1024)
+		op, task_id = data.split(' ')
+		try:
+			ops_map[op](task_id, skt_lock)
+		except Exception as e:
+			print('\nErrorCode: %s'%(e))
+			print('\"%s\" from %s'%(data, addr))
+			with skt_lock:
+				skt.sendto('-1'+' '+task_id, udp_tx_setup)
+			pass
+		pass
+	pass
 
 if __name__ == '__main__':
-	global skt, thread_map
-	main()
+	print("Network Interface Initializing, please wait...")
+	null_ip = "0.0.0.0"
+	bc_ip = "255.255.255.255"
+	bc_mac = "ff:ff:ff:ff:ff:ff"
+	lo_mac = get_if_hwaddr(conf.iface)
+	dhcp_bc_header = (
+		Ether(src=lo_mac,dst="ff:ff:ff:ff:ff:ff")/ \
+		IP(src="0.0.0.0",dst="255.255.255.255")/ \
+		UDP(sport=68,dport=67)
+	)
 
+	udp_tx_setup = ('localhost', 11111)
+	udp_rx_setup = ('localhost', 11112)
+
+	proc_map = {}
+	ops_map = {
+		'0' : release,
+		'1' : request
+	}
+
+	print("Now Running over %s ..."%( repr(conf.iface) ))
+	main()
+	pass
--- test.py
+++ test.py
@@ -1,65 +1,43 @@
 #! /usr/bin/python
+from scapy.all import *
 
-import socket
-
-udp_setup = ('localhost', 11112)
-
-def main():
-	skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	skt.bind(udp_setup)
-
-	while True:
-		print("Waiting for a new command:")
-		data, addr = skt.recvfrom(1024)
-		print('Received:',data,'from',addr)
-		pass
-
-	pass
-
-if __name__ == '__main__':
-	main()
+UDP_BASE_BOOTP_HEADER = 8
+BOOTP_BASE_XID_Offset = 4
+BOOTP_BASE_XID_Length = 4
 
 null_ip = "0.0.0.0"
 bc_ip = "255.255.255.255"
 bc_mac = "ff:ff:ff:ff:ff:ff"
-bootp_xid = random.randint(0, sys.maxint)
+proxy_mac = RandMAC("00:28:f8:7c")
+bootp_xid = random.randint(0, 900000000)
 
-'''
-DHCP Discover Packet
-'''
-l2 = Ether(dst=bc_mac, src=proxy_mac, type=0x0800)
-l3 = IP(src=null_ip, dst=bc_ip)
-udp =  UDP(dport=67, sport=68)
-bootp = BOOTP(op=1, xid=bootp_xid)
-dhcp = DHCP(options=[('message-type','discover'), 
-					 ('end')])
-packet = l2/l3/udp/bootp/dhcp
+m = RandMAC("00:28:f8:7c")
+myxid = random.randint(1, 900000000)
+mymac = get_if_hwaddr(conf.iface)
 
-'''
-DHCP Offer Packet
-'''
-if req.haslayer(BOOTP):
-	bootp = req.getlayer(BOOTP)
-	if bootp.xid == self.__xid:
-		if req.haslayer(DHCP) and self.__ip is None:
-			print "Dhcp packet!"
-			dhcp = req.getlayer(DHCP)
-			if dhcp.options[0][0] == 'message-type':
-				message_type = dhcp.options[0][1]
-				# Only interested in offers
-				if message_type == 2:
-					return 1
+global result
 
+dhcp_discover = (
+	Ether(src=mymac,dst="ff:ff:ff:ff:ff:ff")/ \
+	IP(src="0.0.0.0",dst="255.255.255.255")/ \
+	UDP(sport=68,dport=67)/ \
+	BOOTP(chaddr=[mac2str(m)],xid=myxid,flags=0xFFFFFF)/ \
+	DHCP(options=[('message-type','discover'), 
+				  ('end')])
+)
+
+set_filter = ('udp and udp[%d:%d]=%d'%(12, 4, myxid))
+print(set_filter)
+
+sendp(dhcp_discover, iface=conf.iface)
+sniff(iface=conf.iface, \
+			filter=set_filter, \
+			count=1, timeout=5, \
+			prn=lambda x:x[DHCP].options[3][1])
 '''
-DHCP Request Packet
+pkt[DHCP].options[0][1]#server_id, 192.168.1.1
+pkt[DHCP].options[0][2]#lease_time
+pkt[DHCP].options[0][3]#subnet_mask
+pkt[DHCP].options[0][4]#router
+pkt[DHCP].options[0][5]name_server
 '''
-l3 = Ether(dst=req.getlayer(Ether).src, src=self.__mac)
-l2 = IP(src=self.__ip, dst=req.getlayer(IP).src)
-udp = UDP(sport=req.dport, dport=req.sport)
-bootp = BOOTP(op=1, chaddr=self.__mac, xid=self.__xid)
-dhcp = DHCP(options=[('message-type','request'),
-					 ('client_id', self.__mac),
-					 ('requested_addr', self.__ip),
-					 ('server_id', self.__router),
-					 ('end')])
-rep=l3/l2/udp/bootp/dhcp
